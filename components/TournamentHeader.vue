b1049b4e6b745eee24b6d87e8edf851da3770fe1
coubard alexis <acoubard@cmimedia.fr>
2023-09-06 08:43:50 +0200

    changement de la disposition de présentation d'un tournoi

--- components/TournamentHeader.vue
+++ components/TournamentHeader.vue
@@ -3,9 +3,16 @@
     <div class="container">
       <div class="record-tournament--resume-area">
         <div class="row resume-wrapper">
-          <div class="col-lg-8 col-md-8 justify-content-sm-center d-grid">
-            <h3>{{ tournament.libelle }}</h3>
-            <div class="title-bottom d-flex">
+          <nuxt-img src="images/game-img-1.png" class="col-lg-2"></nuxt-img>
+          <div class="col-lg-10 col-md-10 d-grid">
+            <div class="d-inline-flex justify-content-between">
+              <h3>{{ tournament.libelle }}</h3>
+              <TournamentButton
+                v-if="isOpen(tournament) && isLogged()"
+                :tournament="tournament"
+              />
+            </div>
+            <div class="title-bottom d-flex align-items-end">
               <State :tournament="tournament" />
               <div
                 class="start-area bg--action"
@@ -18,27 +25,6 @@
                 >
               </div>
             </div>
-          </div>
-          <div class="col-lg-3 col-md-4 text-center" v-if="isOpen(tournament) && isLogged()">
-            <NuxtLink
-              :to="{ name: 'tournois-id-register' }"
-              class="cmn-btn register-btn"
-              v-if="!isRegister(tournament) && isOpen(tournament)"
-              >S'inscrire</NuxtLink
-            >
-            <span
-              v-else-if="!isCompletlyClose(tournament) && isRegister(tournament)"
-              class="cmn-btn"
-              @click="unsubscribeClick()"
-              >
-              <template v-if="!unsubscribeLoad">Se desinscrire</template>
-              <template v-else>
-                <div class="spinner-border text-warning" role="status">
-                  <span class="visually-hidden">Loading...</span>
-                </div>
-              </template>
-            </span
-            >
           </div>
         </div>
         <ul class="nav nav-tabs" id="myTab" role="tablist">
@@ -64,43 +50,25 @@ import {useTournamentStore} from "~/store/tournament";
 import State from '@/components/tournament/State.vue'
 import { useMatchStore } from "~/store/match";
 import useSecurity from "~/composables/useSecurity";
+import TournamentButton from "~/components/tournament/TournamentButton.vue";
 
-const { isOwner, isHalf, isRegister, hasMatches, isCompletlyClose, isOpen } =
+const { isOwner, isHalf, hasMatches, isOpen } =
     useTournamentHeader()
 const { addMessage } = useFlashMessages()
 
-const { isLogged, getUser } = useSecurity()
+const { isLogged } = useSecurity()
 
 const { data: auth } = useAuth()
-const tournamentStore = useTournamentStore()
+const { currentTournament: tournament, start, setCurrentTournament } = useTournamentStore()
 const matchStore = useMatchStore()
 
 const bracketLoading = ref(false)
-const unsubscribeLoad = ref(false)
-const tournament = computed(() => tournamentStore.currentTournament)
 
-function unsubscribeClick() {
-  unsubscribeLoad.value = true
 
-  tournamentStore.unsubscribe(tournamentStore.currentTournament.id, getUser().id).then((data) => {
-    tournamentStore.setCurrentTournament(data.data)
-    unsubscribeLoad.value = false
-    addMessage({
-      class: 'success',
-      message: 'Vous n\'etes plus inscrit a ce tournoi.'
-    })
-  }).catch(() => {
-    addMessage({
-      class: 'error',
-      message: 'Une erreur est survenu lors de la désinscription au tournoi.'
-    })
-    unsubscribeLoad.value = false
-  })
-}
 async function generate() {
     bracketLoading.value = true;
-    await tournamentStore.start(tournamentStore.currentTournament).then((data) => {
-      tournamentStore.setCurrentTournament(data.data)
+    await start(tournament).then((data) => {
+      setCurrentTournament(data.data)
       addMessage({
         class: "success",
         message: "Génération de l'arbre réussi.",
---
+++ components/tournament/TournamentButton.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <NuxtLink
+      :to="{ name: 'tournois-id-register' }"
+      class="cmn-btn register-btn"
+      v-if="!isRegister(tournament) && isOpen(tournament)"
+    >S'inscrire
+    </NuxtLink>
+    <span
+      v-else-if="!isCompletlyClose(tournament) && isRegister(tournament)"
+      class="cmn-btn"
+      @click="unsubscribeClick()"
+    >
+      <template v-if="!unsubscribeLoad">Se désinscrire</template>
+      <template v-else>
+        <div class="spinner-border text-warning" role="status">
+          <span class="visually-hidden">Loading...</span>
+        </div>
+      </template>
+    </span>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useTournamentStore } from "~/store/tournament";
+import useFlashMessages from "~/composables/useFlashMessages";
+import useSecurity from "~/composables/useSecurity";
+import { TournamentModel } from "~/app/models/tournament";
+
+const props = defineProps<{tournament: TournamentModel}>()
+const { isRegister, isOpen, isCompletlyClose } = useTournamentHeader()
+
+const { unsubscribe, setCurrentTournament, } = useTournamentStore()
+const { addMessage } = useFlashMessages()
+const { getUser } = useSecurity()
+
+const unsubscribeLoad = ref(false)
+
+function unsubscribeClick() {
+  unsubscribeLoad.value = true
+
+  unsubscribe(props.tournament.id, getUser().id).then((data) => {
+    setCurrentTournament(data.data)
+    unsubscribeLoad.value = false
+    addMessage({
+      class: 'success',
+      message: 'Vous n\'etes plus inscrit a ce tournoi.'
+    })
+  }).catch(() => {
+    addMessage({
+      class: 'error',
+      message: 'Une erreur est survenu lors de la désinscription au tournoi.'
+    })
+    unsubscribeLoad.value = false
+  })
+}
+</script>
+
+<style scoped>
+
+</style>
