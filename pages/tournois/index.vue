7dceabe8f8a6efd5ed1a3631c0c73b67dfc81aad
coubard alexis <acoubard@cmimedia.fr>
2023-09-06 08:45:47 +0200

    fix du probleme du non chargement des 5 premiers contenus sur le listing des tournois

--- pages/tournois/index.vue
+++ pages/tournois/index.vue
@@ -14,6 +14,7 @@
             />
           </transition-group>
           <AppInfiniteScroll
+            v-show="!pending"
             @load="tournamentStore.fetchNextItems()"
             :key="'infiniteKey' + infiniteKey"
           />
